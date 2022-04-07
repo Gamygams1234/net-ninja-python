de432a9a2c66200cbc85a98bee97447e29c96d6b
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-04-07 15:47:51 -0700

    Working on scope

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def greet(x = "John"):
-  print("hello " + x)
+# variable scope
+# global scop
+my_name = "Gamy"
 
+def print_name():
+  # can be overriden
+  #we can update it to go global my_name then my_name = Jonn
+  my_name = "John"
+  print("The name inside the fuction is", my_name)
 
-greet("Gamy")
-# passes John as a default
-greet()
+  
+
+print("the name outside the function is", my_name)
+
+print_name()
