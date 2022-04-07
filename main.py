2a013b6aaa9e78bf48c651e795afe6e41a9f52f7
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-04-07 15:41:54 -0700

    function basics

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-# for n in range(5):
-#   print(n)
+def greet(x = "John"):
+  print("hello " + x)
 
 
-# from 3 to 9
-for n in range(3, 10):
-  print(n)
-
-
-# going  by 4 including the 0
-# for n in range(0, 20,4):
-#   print(n)
-
-burgers = ['beef', "checken", 'birria']
-
-
-for n in range(len(burgers)):
-  print(burgers[n])
-
-# this is going backward in the range
-for n in range(len(burgers)-1, -1, -1):
-  print(burgers[n])
+greet("Gamy")
+# passes John as a default
+greet()
