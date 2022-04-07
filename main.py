1ca7a2f7666a82c963cc76c209b5f87ae49c1c66
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-04-07 07:19:40 -0700

    numbers

---
+++ main.py
@@ -0,0 +1,17 @@
+#  everything in python is considered an object
+
+# integer
+print(type(7))
+# float
+print(type(7.5))
+
+# returns int
+5*5
+# returns int
+5+5
+# returns int
+5-5
+# returns float
+5/5
+# returns int and still 5
+5//5
