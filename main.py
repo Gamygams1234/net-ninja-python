55d1606f9bc061f6152683e2a6369395dac7ea9e
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-04-07 22:29:36 -0700

    methods common and static methods

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Planet:
+  shape = "round"
   def __init__(self, name, radius, gravity,system):
     self.name = name
     self.radius = radius
@@ -8,10 +9,31 @@ class Planet:
   def orbit(self):
     print(self.name+ " is orbiting")
 
+  # this is a class method that only takes in the class variables
+  @classmethod
+  def commons(cls):
+    return f'All planets are {cls.shape} because of gravity'
+  @staticmethod
+  def spin(speed= "2000 mph"):
+    return f'The planet spins at {speed}'
+    
+
 
 hoth = Planet("Hoth",200000,5.5, "Hoth Systems")
+earth = Planet("Earth",200000,5.5, "The government")
 
 print(hoth.name)
 print(hoth.radius)
 print(hoth.gravity)
+print(hoth.shape)
+
+
+print("The earth shape is " +hoth.shape)
 hoth.orbit()
+# this is the commons method amongst all of them
+print(earth.commons())
+
+# this is the static methods that can be overridden
+print (earth.spin("400mph"))
+
+print (hoth.spin())
