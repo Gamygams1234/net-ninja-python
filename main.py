d9c39bd53c5df242d712f4c4b09d1ef0efda2467
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-04-07 17:02:02 -0700

    making a class with the init function

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 class Planet:
-  def __init__(self):
-    self.name = "Hoth"
-    self.radius = 200000
-    self.gravity = 5.5
-    self.system = "Hoth Systems"
+  def __init__(self, name, radius, gravity,system):
+    self.name = name
+    self.radius = radius
+    self.gravity = gravity
+    self.system = system
 
   def orbit(self):
     print(self.name+ " is orbiting")
 
 
-hoth = Planet()
+hoth = Planet("Hoth",200000,5.5, "Hoth Systems")
 
 print(hoth.name)
 print(hoth.radius)
